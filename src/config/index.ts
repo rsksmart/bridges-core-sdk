0d1214befb22ef04f4f93c6e85445841dfc1bade
Luisfc68 <luisfc68@gmail.com>
2025-03-07 19:27:44 -0300

    feat: update flyover config to use Connection abstraction

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-import { type BlockchainConnection } from '..'
+import { type Connection } from '..'
 
 /** Available network options */
 export type Network = 'Mainnet' | 'Testnet' | 'Regtest' | 'Alphanet' | 'Development'
@@ -13,11 +13,12 @@ export interface BridgesConfig {
   allowInsecureConnections?: boolean
   /**
      * Is an object that represnts the connection to the RSK network. It may be obtained using {@link BlockchainConnection.createUsingStandard},
-     * {@link BlockchainConnection.createUsingEncryptedJson} or {@link BlockchainConnection.createUsingPassphrase}. This configuration is optional because
-     * it is intended to be used on operations that require direct connection to the Liquidity Bridge Contract from SDK, users who are only
-     * interested on operations that use Liquidity Provider Server don't have to provide it
+     * {@link BlockchainConnection.createUsingEncryptedJson}, {@link BlockchainReadOnlyConnection.createUsingRpc}} or
+     * {@link BlockchainConnection.createUsingPassphrase}. This configuration is optional because it is intended to be used on operations
+     * that require direct connection to the Liquidity Bridge Contract from SDK, users who are only interested on operations that use
+     * Liquidity Provider Server don't have to provide it
      */
-  rskConnection?: BlockchainConnection
+  rskConnection?: Connection
 }
 
 export * from './flyover'
